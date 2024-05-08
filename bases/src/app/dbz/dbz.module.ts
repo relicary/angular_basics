e2a64b4f0c4346e34f4eb1803ef9a6ec7b07d28c
Juan Cremades <relicary@gmail.com>
2024-05-08 17:41:20 +0200

    Add Character form extracted from Main Page as a Component
    Refs #5

--- bases/src/app/dbz/dbz.module.ts
+++ bases/src/app/dbz/dbz.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    AddCharacterComponent
   ],
   exports: [
     MainPageComponent
